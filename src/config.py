3c939ca5961210e8bac65886666b46ed030d0171
opeanc <ccolle.nicolo@gmail.com>
2026-05-30 16:15:38 +0200

    - Refactor: 'extract_embeddings' function has been split into 'extract_visual_embeddings' and 'extract_text_embeddings' to improve mantainability.
    - Completed 'extract_text_embeddings' and 'extract_visual_embeddings'
    
    - Moved function 'load_vlm' to 'utils.py' from 'extract_embeddings.py'
    
    - Improved 'config.py' by adding fields useful to text processing
    
    - Added 'datasets/iu_xray.py', it contains Dataset classes to handle IU XRay Images and Reports
    
    - Added 'tests/test_extract_embeddings.py' to test  'extract_visual_embeddings' and 'extract_text_embeddings' correct functioning
    
    - Minor changes on gitignore

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 # config.py
 from dataclasses import dataclass
+from pathlib import Path
 
 @dataclass
 class VLMConfig:
@@ -8,10 +9,21 @@ class VLMConfig:
     model_name: str = "chuhac/BiomedCLIP-vit-bert-hf"
     processor_name: str = "chuhac/BiomedCLIP-vit-bert-hf"
     batch_size: int = 64
+    num_workers: int = 4
+    device: str = "cuda"
 
     # Data parameters
     image_dir: str = "data/iu_xray/images/images_normalized"
-    image_ext: str = "*.png"
+    reports_dir: str = "data/iu_xray/reports"
+
     output_dir: str = "embeddings"
     visual_output_filename: str = "visual_embeddings.pt"
-    textual_output_filename: str = "textual_embeddings.pt"
+    text_output_filename: str = "text_embeddings.pt"
+
+    @property
+    def visual_output_path(self) -> Path:
+        return Path(self.output_dir) / self.visual_output_filename
+        
+    @property
+    def text_output_path(self) -> Path:
+        return Path(self.output_dir) / self.text_output_filename
--- src/extract_embeddings.py
+++ src/extract_embeddings.py
@@ -1,70 +1,42 @@
 import torch
-from transformers import AutoModel, AutoProcessor
-from pathlib import Path
-from PIL import Image
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 
 from config import VLMConfig
 
-def load_vlm(config: VLMConfig):
-    """    
-    Args:
-        config (VLMConfig): dataclass containing model parameters.
-        
-    Returns:
-        tuple: (model, processor) loaded.
-    """    
-    model = AutoModel.from_pretrained(
-        config.model_name, 
-        trust_remote_code=True
-    )
-    processor = AutoProcessor.from_pretrained(
-        config.processor_name, 
-        trust_remote_code=True
-    )
-    
-    model.eval().to("cuda")
-    
-    return model, processor
-
-
-def extract_embeddings(model, processor, config: VLMConfig):
+def extract_visual_embeddings(model, processor, dataset: Dataset, config: VLMConfig):
     """
-    It extracts and save embeddings by processing samples from the specified folder.
+    It extracts and save embeddings by processing images from the specified folder.
 
     Args:
         model: loaded model
         processor: loaded processor
-        config (VLMConfig): dataclass containing model parameters.
+        dataset: the dataset you want to extract features on
+        config (VLMConfig): dataclass containing parameters.
     """
-
-    # Visual Embeddings
-    img_dir = Path(config.image_dir)
-    if not img_dir.exists():
-        raise FileNotFoundError(f"Error: Folder {img_dir} not found!")
-    
-    image_paths = sorted(list(img_dir.glob(config.image_ext)))
-    
-    if not image_paths:
-        raise ValueError(f"No image found having extension '{config.image_ext}' in {img_dir}")
         
-    print(f"Found {len(image_paths)} images. Starting extraction...")
+    print(f"\nFound {len(dataset)} images. Starting extraction...")
 
-    config.output_path.parent.mkdir(parents=True, exist_ok=True)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=config.batch_size,
+        shuffle=False,
+        num_workers=config.num_workers,
+        collate_fn=lambda x: tuple(zip(*x))
+    )
 
+    config.visual_output_path.parent.mkdir(parents=True, exist_ok=True)
     all_embeddings = []
 
     model.eval()
     with torch.no_grad():
-        for i in tqdm(range(0, len(image_paths), config.batch_size), desc="Images Feature Extraction"):
-            batch_paths = image_paths[i : i + config.batch_size]
-            
-            # Loading images
-            images = [Image.open(p).convert("RGB") for p in batch_paths]
-            
+        for batch_images, _ in tqdm(dataloader, desc="Images Processing"):
+    
             # Input preparation
-            inputs = processor(images=images, return_tensors="pt", padding=True).to(config.device)
+            inputs = processor.image_processor(
+                images=list(batch_images), 
+                return_tensors="pt"
+            ).to(config.device)
             
             # Inference
             outputs = model.get_image_features(**inputs)  # (B, 512)
@@ -74,4 +46,53 @@ def extract_embeddings(model, processor, config: VLMConfig):
             all_embeddings.append(outputs.cpu())
 
     visual_embeddings = torch.cat(all_embeddings)  # (N, 512)
-    torch.save(visual_embeddings, config.output_path)
+    torch.save(visual_embeddings, config.visual_output_path)
+
+    print(f"Images Embedding Extraction completed. Saving on {config.visual_output_path}.")
+
+
+def extract_text_embeddings(model, processor, dataset: Dataset, config: VLMConfig):
+    """
+    It extracts and save embeddings by processing textual reports from the specified folder.
+
+    Args:
+        model: loaded model
+        processor: loaded processor
+        dataset: the dataset you want to extract features on
+        config (VLMConfig): dataclass containing parameters.
+    """
+    print(f"\nFound {len(dataset)} reports. Starting extraction...")
+    
+    dataloader = DataLoader(
+        dataset,
+        batch_size=config.batch_size, 
+        shuffle=False, 
+        num_workers=config.num_workers,
+        collate_fn=lambda x: tuple(zip(*x))
+    )
+    
+    config.text_output_path.parent.mkdir(parents=True, exist_ok=True)
+    all_embeddings = []
+    
+    model.eval()
+    with torch.no_grad():
+        for batch_texts, _ in tqdm(dataloader, desc="Reports Processing"):
+
+            # Input preparation
+            inputs = processor.tokenizer(
+                text=list(batch_texts), 
+                return_tensors="pt", 
+                padding=True, 
+                truncation=True
+            ).to(config.device)
+
+            # Inference
+            outputs = model.get_text_features(**inputs)
+            outputs = outputs / outputs.norm(dim=-1, keepdim=True)
+
+            # Moving on CPU to avoid VRAM problems
+            all_embeddings.append(outputs.cpu())
+            
+    text_embeddings = torch.cat(all_embeddings)
+    torch.save(text_embeddings, config.text_output_path)
+    print(f"Reports Embedding Extraction completed. Saving on {config.text_output_path}.")
